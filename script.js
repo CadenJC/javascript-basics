b27b225ad37dfe79320598430e91f6fbf1f2a6e2
Easley Collective <111325620+easleycoll@users.noreply.github.com>
2023-03-16 21:38:54 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,52 @@
+// COMPARISON OPERATORS
+// Directions: Use the variables below for the comparison operators questions that follow. Each of your answers below should be in a separate console.log.
+let compA = 5;
+let compB = 8;
+let compC = "8";
+
+// 1. Is compB less than or equal to compA?
+
+// 2. Is compB equal to compC?
+
+// 3. Is compB strictly equal to compC in value and type?
+
+// CONDITIONAL STATEMENTS
+// Directions: The variables below represent the number of minutes 3 different students want to study for each weekday while learning JavaScript. Use the varialbes in the conditional statements you'll write for the following scenarios.
+let goalA = 15;
+let goalB = 30;
+let goalC = 90;
+
+// 1. If student A's goal is less than 30 minutes, console.log the following string: `Student A is studying less than 30 minutes each day. Make sure their study methods are effective.`
+
+// 2. If student B's goal is greater than or equal to 30 minutes, console.log the following string: `Student B's goal is to study 30 minutes or more.`
+// If not, console.log the following string: `Student B is planning to study less than 30 minutes each day. Check on their availability.`
+
+// 3. Use one condtional statement to do the following:
+//If student C's goal is less than 30 minutes, console.log the following string: `Student C is studying less than 30 minutes each day. Make sure their study methods are effective.`
+
+// If student C's goal is less than or equal to 45 minutes, console.log the following string: `Student C's goal is to study between 30 and 45 minutes. Keep encouraging them.`
+
+// If neither of those things are true, console.log the following string: `Student C's goal is to study more than 45 minutes each day. Check in on their capacity.`
+
+// FUNCTIONS
+
+// 1. Write a function that prints, "Hello World", in the console
+
+// 2. Write a function named calculateDogAge that:
+// - takes 1 argument: your puppy's age.
+// - calculates your dog's age based on the conversion rate of 1 human year to 7 dog years.
+// - outputs the result to the screen like so: "Your doggie is NN years old in dog years!"
+// - Call the function three times with different sets of values.
+
+// 3. Write a function named tellFortune that:
+// - takes 4 arguments: number of children, partner's name, geographic location, job title.
+// - outputs your fortune to the screen like so: "You will be a X in Y, and married to Z with N kids."
+// - call that function 3 times with 3 different values for the arguments.
+
+// LOOPS
+
+// 1. Make a loop that counts from 0 to 10.  It should print out "The number is" and each integer, from 0 to 10, in the console.
+
+// 2. Make a loop that counts from 5 to 15 in the console, in multiples of 3. It should print out the number "is a multiple of 3."
+
+// 3. Make a loop that counts backwards from 100 to 0, and prints out in the console.
